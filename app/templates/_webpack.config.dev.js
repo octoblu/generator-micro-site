b7f50d8d48dc931970d384031f59c2b3fd2f719c
sqrt <sqrt@octoblu.com>
2016-06-06 15:31:56 -0700

    v3.1.0 Remove base64:5 ing the CSS or something like that

--- app/templates/_webpack.config.dev.js
+++ app/templates/_webpack.config.dev.js
@@ -25,8 +25,8 @@ module.exports = {
       include: path.join(__dirname, 'src')
     },
     {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader?modules&localIdentName=[name]__[local]___[hash:base64:5]&importLoaders=1!postcss-loader',
+      test:   /\.css$/,
+      loader: "style-loader!css-loader!postcss-loader"
     }]
   },
   postcss: function () {
