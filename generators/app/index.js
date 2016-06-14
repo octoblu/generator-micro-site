ffcbe0a44e95f4db02d2d73916dfa8457652e692
ASteinheiser <asteinhe@asu.edu>
2016-06-14 13:44:43 -0700

    paths again... v4.0.3

--- generators/app/index.js
+++ generators/app/index.js
@@ -39,25 +39,25 @@ module.exports = generators.Base.extend({
       author: this.author,
       githubUrl: this.githubUrl
     }
-    this.template('_webpack.config.dev.js', '/webpack.config.dev.js', context);
-    this.template('_webpack.config.prod.js', '/webpack.config.prod.js', context);
-    this.template('_package.json', '/package.json', context);
-    this.template('_index.html', '/index.html', context);
-    this.template('_dev-server.js', '/dev-server.js', context);
-    this.template('_babelrc', '/.babelrc', context);
-    this.template('_eslintrc', '/.eslintrc', context);
-    this.template('_eslintignore', '/.eslintignore', context);
-    this.template('src/components/NotFound/_NotFound.js', '/src/components/NotFound/NotFound.js', context);
-    this.template('src/components/NotFound/_NotFound.spec.js', '/src/components/NotFound/NotFound.spec.js', context);
-    this.template('src/components/NotFound/_NotFound.css', '/src/components/NotFound/NotFound.css', context);
-    this.template('src/components/NotFound/_index.js', '/src/components/NotFound/index.js', context);
-    this.template('src/config/_routes.js', '/src/config/routes.js', context);
-    this.template('src/containers/_app.js', '/src/containers/app.js', context);
-    this.template('src/containers/_home.js', '/src/containers/home.js', context);
-    this.template('src/_index.js', '/src/index.js', context);
-    this.template('test/_setup.js', '/test/.setup.js', context);
-    this.template('test/_mocha.opts', '/test/mocha.opts', context);
-    this.template('_travis.yml', '/.travis.yml', context);
-    this.template('_gitignore', '/.gitignore', context);
+    this.template('_webpack.config.dev.js', 'webpack.config.dev.js', context);
+    this.template('_webpack.config.prod.js', 'webpack.config.prod.js', context);
+    this.template('_package.json', 'package.json', context);
+    this.template('_index.html', 'index.html', context);
+    this.template('_dev-server.js', 'dev-server.js', context);
+    this.template('_babelrc', '.babelrc', context);
+    this.template('_eslintrc', '.eslintrc', context);
+    this.template('_eslintignore', '.eslintignore', context);
+    this.template('src/components/NotFound/_NotFound.js', 'src/components/NotFound/NotFound.js', context);
+    this.template('src/components/NotFound/_NotFound.spec.js', 'src/components/NotFound/NotFound.spec.js', context);
+    this.template('src/components/NotFound/_NotFound.css', 'src/components/NotFound/NotFound.css', context);
+    this.template('src/components/NotFound/_index.js', 'src/components/NotFound/index.js', context);
+    this.template('src/config/_routes.js', 'src/config/routes.js', context);
+    this.template('src/containers/_app.js', 'src/containers/app.js', context);
+    this.template('src/containers/_home.js', 'src/containers/home.js', context);
+    this.template('src/_index.js', 'src/index.js', context);
+    this.template('test/_setup.js', 'test/.setup.js', context);
+    this.template('test/_mocha.opts', 'test/mocha.opts', context);
+    this.template('_travis.yml', '.travis.yml', context);
+    this.template('_gitignore', '.gitignore', context);
   }
 });
